95bc6d861dcea83d4bcaffe075a6f2823aa3dc13
Kenny Chung <chunghlken@gmail.com>
2022-07-08 12:41:16 -1000

    background and title

--- js/play-scene.js
+++ js/play-scene.js
@@ -3,7 +3,15 @@ class PlayScene extends Phaser.Scene {
         super('game');
     }
 
+    preload() {
+        this.load.image('game-bg', 'assets/background.png');
+    }
+
     create() {
-        this.add.text(20, 20, 'Playing game...', { font: '25px Arial', fill: 'yellow' });
+        this.gameWidth = this.sys.game.config.width;
+        this.gameHeight = this.sys.game.config.height;
+
+        this.gameBg = this.add.tileSprite(0, 0, this.gameWidth, this.gameHeight, 'game-bg');
+        this.gameBg.setOrigin(0, 0);
     }
 }
